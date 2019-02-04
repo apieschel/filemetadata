ef4620ea899fbb907d4070b7a278664db1b5394d
Glitch (receptive-paste) <none>
2019-02-04 21:43:36 +0000

    updates_2_4_19

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
-
 app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
   res.json(//req.file
     {name: req.file.originalname, size: req.file.size, mimetype: req.file.mimetype}
